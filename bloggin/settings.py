6affb28ad6ccb6a8bf635a5563b37a453df8e689
IseFalo <iseskioluwa@gmail.com>
2024-07-25 05:52:51 +0100

    First commit

--- bloggin/settings.py
+++ bloggin/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-poi=hb8j(z!^oa*kwodg1k6o-lbb-hc9u(kb39zwqe+w7_lp@1'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'django-insecure-poi=hb8j(z!^oa*kwodg1k6o-lbb-hc9u(kb39zwqe+w7_lp@1'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower()=="true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 
 # Application definition
@@ -95,6 +96,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 CACHES = {
     "default": {
